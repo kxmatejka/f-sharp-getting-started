30e29bea3a8d2e0c570b9625537ce7c1545189d9
Kryštof Matějka <kx.matejka@gmail.com>
2019-04-06 15:15:46 +0200

    use pattern matching instead of condition

--- intro/sum.fsx
+++ intro/sum.fsx
@@ -1,7 +1,8 @@
 let rec readUserInput numbers =
     let value = System.Console.ReadLine()
-    if value = ":s" then numbers
-    else readUserInput (List.append numbers [value |> int])
+    match value with
+    | value when value = ":s" -> numbers
+    | _ -> readUserInput (List.append numbers [value |> int])
 
 let main () =
     printfn "Write numbers. To calculate sum enter ':s'"
